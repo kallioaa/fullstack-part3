fce607d50fc4dc2ed9a9a7813c886d7226da3bee
kallioaa <aatu.kallio@aalto.fi>
2021-04-27 10:41:21 +0300

    changes to index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 // Morgan token
 morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''));
