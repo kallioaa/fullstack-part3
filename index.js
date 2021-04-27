57041dea5c6b420f8f76fcc354d81c38e9eb7da4
kallioaa <aatu.kallio@aalto.fi>
2021-04-27 18:14:59 +0300

    Part three 2/3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+const Person = require('./models/person');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const person = require('./models/person');
 
 const app = express();
 app.use(cors());
@@ -11,81 +14,53 @@ app.use(express.static('build'));
 morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-];
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => res.json(persons));
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  const id = req.params.id;
+  Person.findById(id).then((person) => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.json(204).end();
+  const id = req.params.id;
+  Person.findByIdAndDelete(id).then(() => res.json(204).end());
 });
 
 app.post('/api/persons', (req, res) => {
-  const randomInt = (max) => Math.floor(Math.random() * max);
-  let person = req.body;
+  const name = req.body.name;
+  const number = req.body.number;
+  console.log(`${name} ${number}`);
 
-  if (!person.name) {
+  if (!name) {
     return res.status(400).json({
       error: 'name missing',
     });
   }
 
-  if (!person.number) {
+  if (!number) {
     return res.status(400).json({
       error: 'number missing',
     });
   }
-
-  if (persons.find((another) => another.name === person.name)) {
-    return res.status(400).json({
-      error: 'name must be uniques',
-    });
-  }
-
-  person = { ...person, id: randomInt(10000) };
-  persons = persons.concat(person);
-  res.json(person);
+  const person = new Person({ name, number });
+  person.save().then((result) => {
+    res.json(result);
+  });
 });
 
 app.get('/info', (req, res) => {
   res.send(`Phonebook has info for ${persons.length} people.<br><br>${new Date()}`);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -1,36 +1,34 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length < 4 && process.argv.length !== 3) {
-  console.log('give password, name, and number as arguments');
+  console.log('give only password or password, name, and number as arguments');
   process.exit(1);
 }
-
-// Schema and model
-const personSchema = new mongoose.Schema({ name: String, number: Number });
-const Person = mongoose.model('Person', personSchema);
 
 // Creating a connection
 const password = process.argv[2];
 const url = `mongodb+srv://fullstack:${password}@fullstack-cluster.iydtm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
+// Schema and model
+const personSchema = new mongoose.Schema({ name: String, number: String });
+const Person = mongoose.model('Person', personSchema);
+
 // Printing the persons in a phonebook
 if (process.argv.length === 3) {
-  console.log('phonebook: ');
   Person.find({}).then((result) => {
+    console.log('phonebook: ');
     result.forEach((person) => {
-      console.log(person);
+      console.log(`${person.name} ${person.number}`);
     });
     mongoose.connection.close();
   });
-  process.exit(1);
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  const person = new Person({ name, number });
+
+  person.save().then((response) => {
+    console.log(response);
+  });
 }
-
-const name = process.argv[3];
-const number = process.argv[4];
-const person = new Person({ name, number });
-
-person.save().then((response) => {
-  console.log(`added ${name} number ${number}`);
-  mongoose.connection.close();
-});
